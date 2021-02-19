163a1a12f04c75008c6a154d3beb95f5201adf3e
chuongmep <31106432+chuongmep@users.noreply.github.com>
2021-02-19 15:33:54 +0700

    re

---
+++ PreviewControlRevitAPI/Previewcontrolcmd.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+namespace PreviewControlRevitAPI
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Previewcontrolcmd : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Application app = uiapp.Application;
+            Document doc = uidoc.Document;
+            FilteredElementCollector collecotr = new FilteredElementCollector(doc);
+            collecotr.OfClass(typeof(Autodesk.Revit.DB.View));
+
+            FrmPreviewWindows win = new FrmPreviewWindows();
+            win.grid1.Children.Add(new PreviewControl(doc, doc.ActiveView.Id));
+            win.ShowDialog();
+            return Result.Succeeded;
+        }
+    }
+}
